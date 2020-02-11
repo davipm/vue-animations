3542225b44961465a31f196dd065bef062221d83
davip <davi.p.m94@gmail.com>
2020-02-11 16:17:58 -0300

    add day 16

--- src/components/Day16.vue
+++ src/components/Day16.vue
@@ -1,7 +1,18 @@
 <template>
   <section class="section">
     <h3 class="section__title">Day 16</h3>
-    <div class="frame frame--fix"></div>
+    <div class="frame frame--fix">
+      <div class="center">
+        <input type="radio" id="check-1" name="rd" />
+        <label class="circle" for="check-1" id="c1"></label>
+        <input type="radio" id="check-2" name="rd" />
+        <label class="circle" for="check-2" id="c2"></label>
+        <input type="radio" id="check-3" name="rd" />
+        <label class="circle" for="check-3" id="c3"></label>
+        <div class="active"></div>
+        <div class="bg"></div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -17,10 +28,83 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   overflow: hidden;
-  background: #28404e;
-  background: -moz-linear-gradient(top, #28404e 0%, #1c2e38 100%);
-  background: -webkit-linear-gradient(top, #28404e 0%, #1c2e38 100%);
-  background: linear-gradient(to bottom, #28404e 0%, #1c2e38 100%);
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#28404e', endColorstr='#1c2e38',GradientType=0 );
+}
+
+.center {
+  position: absolute;
+  text-align: center;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+}
+
+input[type="radio"] {
+  display: none;
+}
+
+.circle {
+  box-sizing: border-box;
+  position: absolute;
+  z-index: 5;
+  top: 175px;
+  left: 115px;
+  width: 50px;
+  height: 50px;
+  border: 2px solid #fff;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+#c2 {
+  left: 175px;
+}
+
+#c3 {
+  left: 235px;
+}
+
+.active {
+  position: absolute;
+  z-index: 10;
+  width: 40px;
+  height: 40px;
+  background: #fff;
+  border-radius: 50%;
+  top: 180px;
+  left: 120px;
+  transition: all 1s ease;
+}
+
+#check-1:checked ~ .active {
+  transform: translateX(0px);
+}
+#check-2:checked ~ .active {
+  transform: translateX(60px);
+}
+#check-3:checked ~ .active {
+  transform: translateX(120px);
+}
+
+.bg {
+  position: absolute;
+  width: 400px;
+  height: 400px;
+  top: 0;
+  left: 0;
+  background: #3498db;
+  border-left: 400px solid #9b59b6;
+  border-right: 400px solid #1abc9c;
+  transition: all 1s ease;
+  transform: translateX(0px);
+}
+
+#check-1:checked ~ .bg {
+  transform: translateX(0px);
+}
+#check-2:checked ~ .bg {
+  transform: translateX(-400px);
+}
+#check-3:checked ~ .bg {
+  transform: translateX(-800px);
 }
 </style>
